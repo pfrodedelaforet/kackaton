681901a6c088b8cb099857dd53ff95d08b57ea53
manonnagy <54953515+manonnagy@users.noreply.github.com>
2020-01-31 10:32:18 +0100

    entree des deplacements

---
+++ monstre.py
@@ -0,0 +1,19 @@
+class Monstre :
+    def __init__(self, position):
+        self.position = position
+    def déplacment(position@, positionD): #ce programme permet de faire déplacer le monstre jusqu'au
+    #personnage
+        while positionD != position@ :
+            if position@[0] <= positionD[0] :
+                positionD[0] -=1
+            elif position@[0] > positionD[0]:
+                positionD[0] += 1
+            elif position@[1] <= positionD[1] :
+                positionD[1] -= 1
+            elif position@[1] > positionD[1] :
+                positionD[1] += 1
+    def apparaitre ():
+        
+                
+
+    
