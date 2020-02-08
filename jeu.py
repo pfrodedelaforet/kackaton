a438b9385271bf60592aeba86e6684c5433d75a5
pfrodedelaforet <pfrodedelaforet@users.noreply.github.com>
2020-02-08 17:36:18 +0100

    ca marche

--- jeu.py
+++ jeu.py
@@ -4,8 +4,8 @@ from random import randrange
 from itertools import product
 import numpy as np
 import time
-from monstre import Monster
-from perso import Perso
+from pygame.locals import *
+from math import sqrt
 WHITE = (255, 255, 255) 
 GREEN = (0, 255, 0) 
 BLUE= (0, 0, 128) 
@@ -67,7 +67,8 @@ M=gener_carte(taille)
 carte = M.carte
 print(M.carte)
 
-
+def dist(pos1, pos2):
+        return sqrt((pos1[0]-pos2[0])**2 + (pos1[1]-pos2[1])**2)
 
 class Perso : 
     def __init__(self, pos, pdv, moulah):#pos 100 * 100
@@ -91,6 +92,9 @@ class Perso :
     def buy_pdv(self, coin):
         self.moulah -= coin
         self.pdv += coin//10
+    def projectile(self, position):
+        return [(int(x * position[0] + (1-x) * self.pos[0]), int(x * position[1] + (1-x) * self.pos[1])) for x in np.arange(0, 150//dist(self.pos, position), 0.04)]
+    
     """
     def quellesalle(self,salle1,salle2,salle3):
         if salle1.c[0]<=self.pos[0]<=salle1.a[0] and salle1.a[1]<=self.pos[1]<=salle1.c[1]:
@@ -112,14 +116,13 @@ class Perso :
         if len(position) == 2 and carte[position[0]][position[1]] == 0: 
             self._pos = position
         elif len(position) != 2 :  
-            raise TypeError("c'est pas une position")
-        elif carte[position[0]][position[1]] != 0 :  
-            raise ValueError("il est pas dans la salle")
+            raise TypeError("c'est pas une position")    
 
 
 class Monster :
-    def __init__(self, monster_pos):
+    def __init__(self, monster_pos, existe = True):
         self._monster_pos = monster_pos
+        self.existe = existe
     @property
     def monster_pos(self):
         return self._monster_pos
@@ -131,40 +134,42 @@ class Monster :
             raise ValueError()
 
     def you_can_fight(self, position):
-        return (self.monster_pos[0]-position[0])**2 + (self.monster_pos[1]-position[1])**2 <= 1
+        if monstre.existe : 
+            return (self.monster_pos[0]-position[0])**2 + (self.monster_pos[1]-position[1])**2 <= 1
 
     def deplacement(self, position): #ce programme permet de faire déplacer le monstre jusqu'au
     #personnage
-        while self.monster_pos != position and not self.you_can_fight(position):
+        if monstre.existe : 
             (x,y) = self.monster_pos
-            if position[0] <= x :
+            if position[0] < x :
                 self.monster_pos = (x-1, y)
             elif position[0] > x :
                 self.monster_pos = (x+1, y)
-            if position[1] <= y :
+            elif position[1] < y and position[0] == x:
                 self.monster_pos = (x, y-1)
-            elif position[1] > y :
+            elif position[1] > y and position[0] == x:
                 self.monster_pos = (x, y+1)
     def projectile(self, position):
-        return [(int(x * position[0] + (1-x) * self.monster_pos[0]), int(x * position[1] + (1-x) * self.monster_pos[1])) for x in np.arange(0, 1, 0.01)]
+        if monstre.existe : 
+            return [(int(x * position[0] + (1-x) * self.monster_pos[0]), int(x * position[1] + (1-x) * self.monster_pos[1])) for x in np.arange(0, 150//dist(self.monster_pos, position), 0.04)]
     #pour coder si le monstre croise un mur on peut utiliser le code de pierrot
    
         
             
 
-    def generator(n): # génère un str de taille n
+    def generator(self, n): # génère un str de taille n
         s = ""
         for k in range(n):
             s = s + chr(97+randrange(26))
         return s
 
-    
+    """
     def fight(self, n = 0) : 
         t_0 = time.time()
-        s= generator(6)
+        s= self.generator(6)
         font = pygame.font.Font('freesansbold.ttf', 32)
-        text = font.render(s, True, green, blue)
-        textRect.center = (1000 // 2, 1000 // 2) 
+        text = font.render(s, True, GREEN, BLUE)
+        textrect.center = (1000 // 2, 1000 // 2) 
         #il faut fermer la fenetre?
         i=0; win = True
         while time.time()-t_0 < 4 and i<6:
@@ -181,8 +186,37 @@ class Monster :
             fight(n)
         else : 
             return n 
-        
-            
+        """
+    def fight(self, n = 0) : 
+        t_0 = time.time()
+        time.sleep(1)
+        print(3)
+        time.sleep(1)
+        print(2)
+        time.sleep(1)
+        print(1)
+        t_0 = time.time()
+        s= self.generator(6)
+        i=0; win = True
+        print(s)
+        while time.time()-t_0 < 4 and i<6:
+            t = time.time() ; t_ = time.time() ; n = 0
+            while  (n == 0) or (a.type != pygame.KEYDOWN and t_-t < 1.5):
+                a = pygame.event.get()
+                t_ = time.time()
+                n+=1
+            a_ = chr(a.key)
+            print(a_)
+            if a_ != s[i] :
+                win = False
+                break
+            i += 1
+        if win == False : 
+            print("nul")
+            n+=1
+            fight(self, n)
+        else : 
+            return n 
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -216,15 +250,17 @@ def apparition_perso(perso):
 monstre = Monster((21,21)) 
 def apparition_monstre(monstre):
     draw_cell(monstre.monster_pos[0], monstre.monster_pos[1], RED)
-
-# while True:
-#     for event in pygame.event.get(): 
-#         if event.type == pygame.QUIT:
-#             pygame.quit()
-#             sys.exit()
-#     pygame.display.update()
-
-
+apparition_monstre(monstre)
+apparition_perso(perso)
+"""
+while True:
+    for event in pygame.event.get(): 
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+"""
+pygame.display.update()
 
 def redraw():
   screen.fill(BLACK)
@@ -235,35 +271,105 @@ def redraw():
             draw_cell(x, y, WHITE)
   apparition_perso(perso)
   apparition_monstre(monstre)
-
-
-
-
-
+  pygame.display.update()
 running = True
+def partial_redraw(projs, projs_monstre, anc_m, anc_p):
+    if monstre.existe :
+        apparition_monstre(monstre) 
+    apparition_perso(perso)
+    if anc_m != monstre.monster_pos : 
+        draw_cell(anc_m[0], anc_m[1], BLACK)
+    if anc_p != perso.pos :
+        draw_cell(anc_p[0], anc_p[1], BLACK)
+    for proj in projs : 
+        if len(proj) == 2 and 0<=proj[1][0] <= 99 and 0<=proj[1][1]<=99:
+            if carte[proj[1][0]][proj[1][1]] == 0 and carte[proj[0][0]][proj[0][1]] == 0:
+                draw_cell(proj[0][0], proj[0][1], BLACK)
+                draw_cell(proj[1][0], proj[1][1], WHITE)
+            elif carte[proj[1][0]][proj[1][1]] != 0 and carte[proj[0][0]][proj[0][1]] == 0:
+                draw_cell(proj[0][0], proj[0][1], BLACK)
+            if monstre.monster_pos == proj[1] : 
+                draw_cell(monstre.monster_pos[0], monstre.monster_pos[1], BLACK)
+                monstre.existe = False
+        elif len(proj) == 1 and 0<=proj[0][0] <= 99 and 0<=proj[0][1]<=99: 
+            if carte[proj[0][0]][proj[0][1]] == 0:
+                draw_cell(proj[0][0], proj[0][1], BLACK)
+        else : 
+            pass
+    for proj in projs_monstre : 
+        if len(proj) == 2 and 0<=proj[1][0] <= 99 and 0<=proj[1][1]<=99:
+            if carte[proj[1][0]][proj[1][1]] == 0 and carte[proj[0][0]][proj[0][1]] == 0:
+                draw_cell(proj[0][0], proj[0][1], BLACK)
+                draw_cell(proj[1][0], proj[1][1], BLUE)
+            elif carte[proj[1][0]][proj[1][1]] != 0 and carte[proj[0][0]][proj[0][1]] == 0:
+                draw_cell(proj[0][0], proj[0][1], BLACK)
+            if perso.pos == proj[1] : 
+                draw_cell(perso.pos[0], perso.pos[1], BLACK)
+                pygame.quit()    
+        elif len(proj) == 1 and 0<=proj[0][0] <= 99 and 0<=proj[0][1]<=99: 
+            if carte[proj[0][0]][proj[0][1]] == 0:
+                draw_cell(proj[0][0], proj[0][1], BLACK)
+    if monstre.existe :
+        apparition_monstre(monstre) 
+    apparition_perso(perso)
+    pygame.display.update()
 
+def anc_new_proj(ens):
+    anc_new = [[]]
+    for t in ens :
+        if t != None :
+            if len(t) > 1:
+                anc_t = t.pop()
+                anc_new.append([anc_t, t[-1]])
+            if len(t) == 1 : 
+                anc_new.append([t.pop()])   
+    return anc_new
+
+
+ens_proj = [[]]
+ens_proj_monstre = [[]]
+
+time_since_last_update = 0
+n = 1
+L = []
 while running:
-  (dx, dy) = (0,0)
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      running = False   
-    elif event.type == pygame.KEYDOWN:
-      if event.key == K_q:
-        running = False
-      elif event.key == K_UP:
-        (dx, dy) = (0, -1)
-      elif event.key == K_RIGHT:
-        (dx, dy) = (1, 0)
-      elif event.key == K_DOWN:
-        (dx, dy) = (0, 1)
-      elif event.key == K_LEFT:
-        (dx, dy) = (-1, 0)
-    print(dx, dy)
-  perso.depl(dx,dy)
-  monstre.deplacement(perso.pos)
-  redraw()
-
-
+    anc_pos_monstre = monstre.monster_pos
+    anc_pos_perso = perso.pos
+    n+= 1
+    (dx, dy) = (0,0)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False   
+        elif event.type == pygame.KEYDOWN:
+            if event.key == K_q:
+                running = False
+            elif event.key == K_UP:
+                (dx, dy) = (0, -1)
+            elif event.key == K_RIGHT:
+                (dx, dy) = (1, 0)
+            elif event.key == K_DOWN:
+                (dx, dy) = (0, 1)
+            elif event.key == K_LEFT:
+                (dx, dy) = (-1, 0)
+            elif event.key == K_SPACE:
+                L = perso.projectile(monstre.monster_pos)
+                L.reverse()
+    if L != ens_proj[-1] or L != []:
+        ens_proj.append(L)
+    anc_new = anc_new_proj(ens_proj) 
+    perso.depl(dx,dy)            
+    if n%2 == 0 : 
+        monstre.deplacement(perso.pos)
+        T = monstre.projectile(perso.pos)
+        if T != None : 
+            T.reverse()
+        if T != ens_proj[-1] or T != []:
+            ens_proj_monstre.append(T)
+    anc_new_monstre = anc_new_proj(ens_proj_monstre)
+    partial_redraw(anc_new, anc_new_monstre, anc_pos_monstre, anc_pos_perso)
+    anc_new = [[]]
+    time.sleep(0.5)
+"""
 
 carte.escalier1(salle1,salle2)
 carte.escalier2(salle2,salle3)
@@ -293,7 +399,7 @@ carte.escalier2(salle2,salle3)
 
 # for k in range(-10,10):
 #     for j in range (-10,10):
-#         screen.set_at((810+k,610+j), RED)
+#         screen.set_at((810+k,610+j), RED)"""
 # 
 # """ le monstre envoie un projectile,scenario """
 # L = monster.projectile(perso.pos)
